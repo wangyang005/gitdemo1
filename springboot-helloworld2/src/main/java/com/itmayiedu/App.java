90189847e6cf475520f371681b77f24e206c9e07
wcj <wangchengjiang10@163.com>
2019-11-05 21:47:54 +0800

    代码初始化

---
+++ springboot-helloworld2/src/main/java/com/itmayiedu/App.java
@@ -0,0 +1,14 @@
+
+package com.itmayiedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+	public static void main(String[] args) {
+		// 主函数运行springboot项目
+		SpringApplication.run(App.class, args);
+	}
+
+}
